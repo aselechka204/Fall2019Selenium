8051d184a694377c372a0c3dbcd2c9733a0ac7f1
aselechka204 <aselechka204@yahoo.com>
2020-03-07 10:35:14 -0600

    RadioButtons

--- src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -1,3 +1,5 @@
+package com.automation.tests.day1;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
---
+++ src/test/java/com/automation/tests/day5/RadioButtons.java
@@ -0,0 +1,33 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class RadioButtons {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        driver.manage().window().maximize();
+
+        BrowserUtils.wait(2);
+        // <input type="radio>
+        List<WebElement> radioButtons = driver.findElements(By.tagName("input"));
+        for(WebElement radioButton: radioButtons){
+            radioButton.click();
+            BrowserUtils.wait(1);
+        }
+
+
+
+        driver.quit();
+
+    }
+}
